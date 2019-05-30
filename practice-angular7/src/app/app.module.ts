3e8c1b9314a0cf4292fa0d0b995eeec00d88d800
Katie Amaral <kathryn_amaral@harvard.edu>
2019-05-30 10:34:21 -0400

    angular.io Table retrieving data through HTTP

--- practice-angular7/src/app/app.module.ts
+++ practice-angular7/src/app/app.module.ts
@@ -3,23 +3,29 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //import { MatButtonModule, MatCheckboxModule, MatMenuModule } from '@angular/material';
 import { MaterialModule } from './material-module';
+//import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { TableComponent } from './components/table/table.component';
+import { TableHttpComponent } from './components/table-http/table-http.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    TableComponent
+    TableComponent,
+    TableHttpComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    //HttpModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
